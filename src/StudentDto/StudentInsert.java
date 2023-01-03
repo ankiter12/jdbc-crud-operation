52c7e0c445feb35e33721436a94d1b3a1759d9ec
ankiter12 <ankitbr44@gmail.com>
2023-01-03 12:59:26 +0530

    jdbc-noida-today-project

---
+++ src/StudentDto/StudentInsert.java
@@ -0,0 +1,16 @@
+package StudentDto;
+
+public class StudentInsert {
+	public static void main(String[] args) {
+	
+		try {
+			class.forName()
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}	
+		
+		
+	}
+
+}
